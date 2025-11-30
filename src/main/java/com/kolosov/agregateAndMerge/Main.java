004f05e04b655d2f7a10fbd4cf98a71f5856722d
Александр Колосов <kolosov.a@red-promo.ru>
2025-12-01 00:40:32 +0300

    StreamApi

--- src/main/java/com/kolosov/agregateAndMerge/Main.java
+++ src/main/java/com/kolosov/agregateAndMerge/Main.java
@@ -3,7 +3,6 @@ package com.kolosov.agregateAndMerge;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
--- src/main/java/com/kolosov/numberGenerator/Main.java
+++ src/main/java/com/kolosov/numberGenerator/Main.java
@@ -1,8 +1,9 @@
 package com.kolosov.numberGenerator;
 
 
-import java.util.*;
-import java.util.function.Supplier;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
